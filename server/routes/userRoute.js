b31b08333fca70c963c657034734d52ad760823b
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-09-23 09:41:05 +0530

    authentication - backend

---
+++ server/routes/userRoute.js
@@ -0,0 +1,81 @@
+const User = require("../models/userModel");
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// user registration
+
+router.post("/register", async (req, res) => {
+  try {
+    // check if user already exists
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.send({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // create new user
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.send({
+      success: true,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
+
+// user login
+
+router.post("/login", async (req, res) => {
+  try {
+    // check if user exists
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.send({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    // check if password is correct
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.send({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    // create and assign a token
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.send({
+      success: true,
+      message: "User logged in successfully",
+      data: token,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
+const usersRoute = require("./routes/userRoute");
+app.use(express.json());
 
 const server = require("http").createServer(app);
+
+app.use("/api/users", usersRoute);
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
