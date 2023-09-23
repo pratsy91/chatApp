614cbd9eeee94471c97c039d3f7abf23e37c5129
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-09-23 21:45:51 +0530

    messages

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
 const usersRoute = require("./routes/userRoute");
 const chatsRoute = require("./routes/chatsRoute");
+const messagesRoute = require("./routes/messagesRoute");
 app.use(cors());
 app.use(express.json());
 
@@ -14,5 +15,6 @@ const server = require("http").createServer(app);
 
 app.use("/api/users", usersRoute);
 app.use("/api/chats", chatsRoute);
+app.use("/api/messages", messagesRoute);
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
