014977bf525d7ad1e5aecaa01c704bdbba6da043
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-09-24 13:44:37 +0530

    clearing chat messages

--- client/src/apicalls/chats.js
+++ client/src/apicalls/chats.js
@@ -19,3 +19,17 @@ export const CreateNewChat = async (members) => {
     throw error;
   }
 };
+
+export const ClearChatMessages = async (chatId) => {
+  try {
+    const response = await axiosInstance.post(
+      "/api/chats/clear-unread-messages",
+      {
+        chat: chatId,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
